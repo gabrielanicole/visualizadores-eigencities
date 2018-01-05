084fe7ff8d8a4be8c898a544cc3a0789b7619503
Gabriela <gabi@ecomac.local>
2018-01-05 14:57:54 -0300

    trayect with map options

--- manzanas/settings.py
+++ manzanas/settings.py
@@ -86,17 +86,18 @@ WSGI_APPLICATION = 'manzanas.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/1.10/ref/settings/#databases
-DATABASE_ROUTERS = ['trayect.router.TrayectDatabaseRouter']
+#DATABASE_ROUTERS = ['trayect.router.TrayectDatabaseRouter']
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'CENSO02_final.db'),
     }
-    'trayactoria_db':{
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'trayectoria.db'),
+    
+    # 'trayactoria_db':{
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': os.path.join(BASE_DIR, 'trayectoria.db'),
         
-    }
+    # }
     #opcion con default en mongo
     # 'default': {}
     # 'sqlite' : {'ENGINE': 'django.db.backends.sqlite3',
--- trayect/admin.py
+++ trayect/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
+from trayect.models import Ciudad, Indicador, CiudIndic, Antena, AntIndic, Estadia, Desplazamiento
+
 # Register your models here.
+admin.site.register(Ciudad)
+admin.site.register(Indicador)
+admin.site.register(CiudIndic)
+admin.site.register(Antena)
+admin.site.register(AntIndic)
+admin.site.register(Estadia)
+admin.site.register(Desplazamiento)
+

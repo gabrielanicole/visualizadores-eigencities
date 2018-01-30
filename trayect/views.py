90cee7928f37775842b888b7d3b6af43c1d2c0f8
gabrielanicole <gabriela.gonzalezsaez@gmail.com>
2018-01-30 16:05:49 -0300

    data of trayect app

--- trayect/views.py
+++ trayect/views.py
@@ -2,7 +2,7 @@ import json
 from django.core.serializers.json import DjangoJSONEncoder
 from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
-from trayect.models import Ciudad, Indicador, CiudIndic, Antena, AntIndic, Estadia, Desplazamiento
+from trayect.models import Ciudad, Indicador, CiudIndic, Antena, AntIndic, Estadia, Desplazamiento, Persona
 from django.core.serializers import serialize
 
 
@@ -11,6 +11,45 @@ from django.core.serializers import serialize
 def index(request):
 	if request.method == 'POST':
 		print("ENPOST ")
+		if 'torreTray' in request.POST:
+			print("antena en torreTray")
+			nom_torre= request.POST['torreTray'] 
+			diaAnalisis  = request.POST['diaAnalisis']
+
+			antena = Antena.objects.filter(nombre = nom_torre)
+			# print(antena[0])
+			antena = antena[0]
+			desplazamientosAntena = Estadia.objects.values('numa_id').distinct().filter(antena = antena, dia = diaAnalisis)
+
+			# print(desplazamientosAntena)
+			tray = []
+			for user in desplazamientosAntena:
+				print(user['numa_id'])
+				persona = Persona.objects.get(id = user['numa_id'])
+				resd = persona.residencia
+				indicadores = AntIndic.objects.filter(antena = resd).values('indicador__nombre', 'cantidad')
+				print("indicadores")
+				print(indicadores)
+				# mydict.keys()[mydict.values().index(16)]
+				ABC1 = [x['cantidad'] for x in indicadores if x['indicador__nombre'] == 'ABC1']
+				print(ABC1)
+				nse = {"ABC1": 0, "C2":0 , "C3": 0, "D": 0, "E": 0}
+				for key in nse:
+					print(key)
+					ind = [x['cantidad'] for x in indicadores if x['indicador__nombre'] == key]
+					nse[key] = ind[0] if len(ind) == 1 else 0 
+				print("nse")
+				print(nse)
+
+				
+				estadiasPersona = Estadia.objects.filter(numa = persona, dia = diaAnalisis).values_list('antena__lat', 'antena__lon', 'horaP',).order_by('horaP')
+				# print("estadiasPersona")
+				# print(list(estadiasPersona))
+				tray.append({ "numa":persona.numa, "moves": list(estadiasPersona), "NSE" : nse })
+ 
+			# print(tray)
+			return JsonResponse({'results': tray}, safe=False)
+
 		if 'cidadTorres' in request.POST:
 			id_ciudadPOST= request.POST['cidadTorres'] 
 			antenas = Antena.objects.filter(ciudad = id_ciudadPOST)

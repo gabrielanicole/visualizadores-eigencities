ccacad715473884cb0b4b7d84a84d56e769923ad
gabrielanicole <gabriela.gonzalezsaez@gmail.com>
2018-11-23 02:34:20 -0300

    grafico escalamiento hasta modelo

--- escalamiento/views.py
+++ escalamiento/views.py
@@ -1,5 +1,6 @@
 import json
 import math 
+import numpy as np
 from django.http import HttpResponse, JsonResponse
 
 from django.shortcuts import render
@@ -13,14 +14,20 @@ def index(request):
 		if 'varSelect' in request.POST:
 			print("Llamada desde cambio de variable")
 			varSelect = request.POST['varSelect']
+			logVar = "log_" + varSelect
 			#if log: 
-			data = list(Comuna_esc.objects.values_list("nombre", "log_pob_Comun_2002" , varSelect ))
-			
+			data = list(Comuna_esc.objects.values_list("nombre", "log_pob_Comun_2002" , logVar, "pob_Comun_2002", varSelect ))
+			data_x = [float(x[1]) for x in data if  x[1] and x[2] and len(str(x[2])) > 3 ]
+			data_y = [float(x[2]) for x in data if  x[1] and x[2] and len(str(x[2])) > 3 ]
+			reg = np.polyfit(data_x, data_y , 1) 
+			data = [ x for x in data if  x[1] and x[2] and len(str(x[2])) > 3 ]
+			#data = dict(Comuna_esc.objects.values_list("nombre", "log_pob_Comun_2002" , varSelect ))
+			print(reg)
 			#If logaritmo: --> para implmentar en el futuro si no es simpre en log. 
 			#data_log = [ (x[0], x[1], math.log(x[2]) ) for x in data if type(x[2]) != float  ]
 			#print(data_log)
 			print("enviado")
-			return JsonResponse({'results': data }, safe=False)
+			return JsonResponse({'results': data, 'reg': list(reg) }, safe=False)
 
 
 	ciudades = Comuna_esc.objects.all()
